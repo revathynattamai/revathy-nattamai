0991ce60584f34c377bf9d83f46c32bde5bb67f5
revathynattamai <revathy.ng@gmail.com>
2018-12-31 11:59:00 +0100

    Fixes image animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-
-function root() {
-    let div = document.getElementById('root');
+const arr = ["images/carousel_1.jpg", "images/carousel_2.jpg", "images/carousel_3.jpg", "images/hero.jpg"];
+let counter = 0;
+window.onload = function () {
+    setInterval(function () { getImage() }, 3000);
 }
+
+function getImage() {
+    if (counter === arr.length ) counter = 0;
+    let newImage = document.getElementById('abc');
+    newImage.src = arr[counter];
+    counter++;
+}
+
