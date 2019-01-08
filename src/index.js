80949110f0b8bc4c9f7828d327221ec8e4ca6cd8
Revathy Nattamai <revathy.gopinath@wipro.com>
2019-01-08 17:16:30 +0000

    Fixes hash change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-const arr = ["images/carousel_1.jpg", "images/carousel_2.jpg", "images/carousel_3.jpg", "images/hero.jpg"];
+const arr = ["images/carousel_2.jpg", "images/carousel_3.jpg", "images/carousel_1.jpg"];
+const link = ["Home", "Career", "Skills", "Contact"];
 let counter = 0;
 window.onload = function () {
-    setInterval(function () { getImage() }, 3000);
+    setInterval(function () { getImage() }, 5000);
+    addFooter();
 }
 
 function getImage() {
-    if (counter === arr.length ) counter = 0;
+    if (counter === arr.length) counter = 0;
     let newImage = document.getElementById('abc');
     newImage.src = arr[counter];
     counter++;
 }
+
+function addFooter() {
+    let footer = document.getElementById('footer');
+    for (let i = 0; i <= link.length - 1; i++) {
+        let newLink = document.createElement("a");
+        newLink.href = "#" + link[i].toLowerCase();
+        var newContent = document.createTextNode(link[i]);
+        newLink.appendChild(newContent);
+        footer.appendChild(newLink);
+    }
+}
+function getCareer() {
+    console.log("career");
+}
+function getSkills() {
+    console.log("skills");
+}
+function getContact() {
+    console.log("contact");
+}
+
+function locationHashChanged() {
+    let url = window.location.href;
+    if (url.indexOf('home') === 78) {
+         getImage();
+    } else if (url.indexOf('career') === 78) {
+         getCareer();
+    } else if (url.indexOf('skills') === 78) {
+         getSkills();
+    } else {
+         getContact();
+    }
+}
+
+window.addEventListener('hashchange', locationHashChanged);
+
+
+
 
